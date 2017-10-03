3d58eeeff4ae17ecd0d2361864936f2bd64fadda
kwahalf <kwanusud@gmail.com>
2017-10-03 10:35:01 +0300

    [chore] add living space model test

---
+++ src/test/java/org/kwahalf/module/model/LivingSpaceModelTest.java
@@ -0,0 +1,34 @@
+package org.kwahalf.module.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LivingSpaceModelTest {
+	
+	private LivingSpace livingSpace;
+	private Person person;
+
+	@Before
+	public void setUp() throws Exception {
+		livingSpace = new LivingSpace("TEST");
+		person = new Fellow("TestPerson", true);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIsOffice() {
+		assertTrue("This is not a living space",livingSpace.getPurpose().equals("LivingSpace") );
+		assertTrue("This is not a living space named TEST",livingSpace.getRoomName().equals("TEST") );
+		assertTrue("Max capacity of a living space is 4 people",livingSpace.getMaxCapacity().equals(4) );
+		livingSpace.setOccupants(person);
+		assertTrue("TestPerson is an occupant", livingSpace.getOccupantsByIndex(0).getPersoName().equals("TestPerson"));
+		
+	}
+
+}
